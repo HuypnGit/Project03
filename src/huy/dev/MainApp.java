d9b08b1457ccc9fa1bd930286e1db91e58d9bb52
unknown <huy.sirmaster@gmail.com>
2018-09-07 16:05:14 +0700

    dssv

---
+++ src/huy/dev/MainApp.java
@@ -0,0 +1,22 @@
+package huy.dev;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class MainApp {
+
+	public static void main (String[]args) {
+		try {
+			FileInputStream fis = new FileInputStream("in.txt");
+
+			BufferedReader br = new BufferedReader (new InputStreamReader(fis));
+			
+			String line ;
+			
+			while ((line = br.readLine()) !=null) {
+				System.out.println(line);
+			}
+			br.close ();
+		} catch (Exception e) {}		
+	}
+}
